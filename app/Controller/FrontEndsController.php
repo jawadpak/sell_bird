2ad27a50d132b1563b662f2e2c1c57c502e79e6f
jawad rehman <jawadrehman@zoniversal.com>
2015-06-05 18:47:44 +0500

    set up Cake PHP

---
+++ app/Controller/FrontEndsController.php
@@ -0,0 +1,22 @@
+<?php
+App::uses('AppController', 'Controller');
+
+class FrontEndsController extends AppController {
+	public $uses = array();
+
+	public function index() {
+		$msg ="Welcome World";
+		$this->set('msg',$msg);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+?>
---
+++ app/View/FrontEnds/index.ctp
@@ -0,0 +1,1 @@
+Hi Junaid <?php echo $msg; ?> 
